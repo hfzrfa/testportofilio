19b56e63234ce68d02d902d71942eef9adfbd4ef
Hafizrafieaditya <73168585+hfzrfa@users.noreply.github.com>
2024-11-25 14:57:15 +0700

    commit

---
+++ src/app/page.js
@@ -0,0 +1,25 @@
+import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
+import ProjectsSection from "./components/ProjectsSection";
+import ArticleSection from "./components/ArticleSection";
+import AboutSection from "./components/AboutSection";
+import EmailSection from "./components/EmailSection";
+import Footer from "./components/Footer";
+import AchievementsSection from "./components/AchievementsSection";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col bg-[#121212]">
+      <Navbar />
+      <div className="container mt-24 mx-auto px-12 py-4">
+        <HeroSection />
+        <AboutSection />
+        <ProjectsSection />
+        <ArticleSection />
+        {/* <AchievementsSection /> */}
+        <EmailSection />
+      </div>
+      <Footer />
+    </main>
+  );
+}
